8c235fe639eb8b66fe46bd3819fefe64a37e4eca
Vinicius Tacana <vini.tacana@gmail.com>
2024-07-09 14:47:41 -0400

    Adicionando as implementações já feitas no arquivo certo de grafos

--- Grafos.cpp
+++ Grafos.cpp
@@ -1,124 +1,155 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "Grafos.h"
 
-No* newNo(int w){
-    No* no= (No*)malloc(sizeof(No));
-    if(no==NULL)
-        return NULL;    
-    no->w= w;
-    no->prox= NULL;
+No *newNo(int w)
+{
+    No *no = (No *)malloc(sizeof(No));
+    if (no == NULL)
+        return NULL;
+    no->w = w;
+    no->prox = NULL;
     return no;
 }
-graph* newGraph(int V){
-    graph* grafo= (graph*)malloc(sizeof(graph));
-    if(grafo){
-        grafo->V= V;
-        grafo->U= 0;
-        grafo->adj= (No**)malloc(V*sizeof(No*));
-        for(int i=0;i<V;i++){
-            grafo->adj[i]=NULL;
+graph *newGraph(int V)
+{
+    graph *grafo = (graph *)malloc(sizeof(graph));
+    if (grafo)
+    {
+        grafo->V = V;
+        grafo->U = 0;
+        grafo->adj = (No **)malloc(V * sizeof(No *));
+        for (int i = 0; i < V; i++)
+        {
+            grafo->adj[i] = NULL;
         }
-
     }
     return grafo;
-
 }
 
-void addAresta(graph* grafo, int v, int w){
-    No* no= newNo(w);
-    no->prox= grafo->adj[v];
-    grafo->adj[v]=no;
+void addAresta(graph *grafo, int v, int w)
+{
+    No *no = newNo(w);
+    no->prox = grafo->adj[v];
+    grafo->adj[v] = no;
 
-    no= newNo(v);
-    no->prox= grafo->adj[w];
-    grafo->adj[w]=no;
+    no = newNo(v);
+    no->prox = grafo->adj[w];
+    grafo->adj[w] = no;
     grafo->U++;
 }
-void exibeGraph(graph* grafo){
-    for(int v=0; v<grafo->V;v++){
-        No* p= grafo->adj[v];
+void exibeGraph(graph *grafo)
+{
+    for (int v = 0; v < grafo->V; v++)
+    {
+        No *p = grafo->adj[v];
         printf("\n Vertice %d: \n", v);
-        while(p){
+        while (p)
+        {
             printf("\n->%d", p->w);
-            p=p->prox;
+            p = p->prox;
         }
         printf("\n");
     }
 }
-void freeGraph(graph* grafo){
-    for(int v=0; v<grafo->V;v++){
-        No*p= grafo->adj[v];
+void freeGraph(graph *grafo)
+{
+    for (int v = 0; v < grafo->V; v++)
+    {
+        No *p = grafo->adj[v];
         while (p)
         {
-        No* aux= p;
-        p=p->prox;
-        free(aux);            
+            No *aux = p;
+            p = p->prox;
+            free(aux);
         }
-        
     }
     free(grafo->adj);
     free(grafo);
 }
-graph* initGraphArq(const char *cam){
-    FILE *arq= fopen(cam, "r");
-    if(arq==NULL)
+graph *initGraphArq(const char *cam)
+{
+    FILE *arq = fopen(cam, "r");
+    if (arq == NULL)
         return NULL;
     int V;
     fscanf(arq, "%d", &V);
-    graph* g= newGraph(V);
+    graph *g = newGraph(V);
     int v, u;
-    while(fscanf(arq, "%d %d", &v, &u)!=EOF){
-        addAresta(g, v-1, u-1);
+    while (fscanf(arq, "%d %d", &v, &u) != EOF)
+    {
+        addAresta(g, v - 1, u - 1);
     }
     fclose(arq);
     return g;
 }
-
-
-//struct grafo{
-  //  Vertice **vertices;
-   // Aresta **arestas;
-//}
-
-/*
-struct graph{
-    int V;
-    int A;
-    int **adj;
-    
-};
-typedef struct graph *Graph;
-// REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função constroiGrafo() constrói um grafo com vértices 0 1 .. V-1 e nenhum arco. 
-
-Graph constroiGrafo(int V){
-    Graph G = malloc(sizeof *G );
-    G->V= V;
-    G->A= 0;
-    G->adj= 
-
-}*/
-
-/* 
-struct node{
-    
-};
-struct arc 
+int numArestasL(graph *grafo)
 {
-    int adj;
-};
-struct grafos
+    int arestas = 0;
+    for (int i = 0; i < grafo->V; i++)
+    {
+        No *aux = grafo->adj[i];
+        while (aux)
+        {
+            arestas++;
+            aux = aux->prox;
+        }
+    }
+    return arestas / 2;
+}
+int numVertL(graph *grafo)
 {
-    struct node nodes[maxnodes];
-  
-};
-struct grafos g; 
+    return grafo->V;
+}
+int grauVertL(graph *grafo, int *graus)
+{
+    for (int i = 0; i < grafo->V; i++)
+    {
+        graus[i] = 0;
+        No *aux = grafo->adj[i];
+        while (aux)
+        {
+            graus[i]++;
+            aux = aux->prox;
+        }
+    }
+}
+int grauMinL(graph *grafo)
+{
+    int graus[grafo->V];
+    grauVertL(grafo, graus);
+    int min = graus[0];
+    for (int i = 1; i < grafo->V; i++)
+    {
+        if (graus[i] < min)
+        {
+            min = graus[i];
+        }
+    }
+    return min;
+}
 
- */
+int grauMaxL(graph *grafo){
+    int graus[grafo->V];
+    grauVertL(grafo, graus);
+    int max=graus[0];
+    for(int i=0;i<grafo->V;i++){
+        if(graus[i]>max){
+          max=graus[i];  
+        }
+        
+    }
+    return max;
 
+}
 
-
-
-
-
+int grauMedioL(graph *grafo){
+    int graus[grafo->V];
+    grauVertL(grafo, graus);
+    int soma=0;
+    for(int i=0;i<grafo->V;i++){
+        soma=soma+graus[i];
+    }
+    return soma/grafo->V;
+}
--- Grafos.h
+++ Grafos.h
@@ -18,6 +18,11 @@ void addAresta(graph* grafo, int v, int w);
 void exibeGraph(graph* grafo);
 void freeGraph(graph* grafo);
 graph* initGraphArq(const char *cam);
-
+int numArestasL(graph *grafo);
+int numVertL(graph *grafo);
+int grauVertL(graph *grafo, int *graus);
+int grauMinL(graph *grafo);
+int grauMaxL(graph *grafo);
+int grauMedioL(graph *grafo);
 
 #endif
